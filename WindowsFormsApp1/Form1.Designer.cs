959d31f17341ee01f10ec370c11da3ecca0de124
bmtx <zw0424007@sohu.com>
2018-06-30 10:41:08 +0800

    系统目录

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,20 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.tvPath = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imgListFlies = new System.Windows.Forms.ImageList(this.components);
+            this.splitter1 = new System.Windows.Forms.Splitter();
             this.SuspendLayout();
+            // 
+            // tvPath
+            // 
+            this.tvPath.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tvPath.ImageIndex = 0;
+            this.tvPath.ImageList = this.imgListFlies;
+            this.tvPath.Location = new System.Drawing.Point(0, 0);
+            this.tvPath.Name = "tvPath";
+            this.tvPath.SelectedImageIndex = 0;
+            this.tvPath.Size = new System.Drawing.Size(270, 368);
+            this.tvPath.TabIndex = 0;
+            this.tvPath.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.tvPath_BeforeExpand);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(309, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // imgListFlies
+            // 
+            this.imgListFlies.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imgListFlies.ImageSize = new System.Drawing.Size(16, 16);
+            this.imgListFlies.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(270, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 368);
+            this.splitter1.TabIndex = 3;
+            this.splitter1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(651, 368);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tvPath);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView tvPath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ImageList imgListFlies;
+        private System.Windows.Forms.Splitter splitter1;
     }
 }
 
